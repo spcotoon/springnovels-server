dcdf833aee9c59425afbc1e90deb293afec33537
spcotoon <springcomicskr@gmail.com>
2025-04-05 03:13:47 +0900

    modify lock

--- src/main/java/com/app/springnovels/api/facade/RedissonLockNovelFacade.java
+++ src/main/java/com/app/springnovels/api/facade/RedissonLockNovelFacade.java
@@ -3,6 +3,7 @@ package com.app.springnovels.api.facade;
 import com.app.springnovels.api.service.novel.NovelService;
 import com.app.springnovels.api.service.novel.response.NovelResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RedissonLockNovelFacade {
@@ -19,15 +21,23 @@ public class RedissonLockNovelFacade {
 
     public NovelResponse getNovel(Long novelId, Long memberId, LocalDateTime purchaseDateTime) {
         RLock lock = redissonClient.getLock(novelId.toString());
-
+        boolean available = false;
 
         try {
-            lock.tryLock(10, 1, TimeUnit.SECONDS);
+            available = lock.tryLock(10, 1, TimeUnit.SECONDS);
+
+            if (!available) {
+                log.info("get lock failed");
+            }
+
              return novelService.getNovel(novelId, memberId, purchaseDateTime);
         } catch (InterruptedException e) {
+            log.info("high traffic, try again please");
             throw new RuntimeException(e);
         } finally {
-            lock.unlock();
+            if (available && lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
         }
     }
 }
